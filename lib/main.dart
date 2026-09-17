527d01ce20c5b10c61181a2351f45bca2b31fdbd
sarapsarap517-ops <sarapsarap517@gmail.com>
2026-09-17 17:22:46 +0300

    Create main.dart for Flutter app setup
    Initialize Flutter app with login screen and theme.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'تطبيق الواي فاي',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
